06c75775632aab453a66c080290395f629933990
dalia101 <54603019+dalia101@users.noreply.github.com>
2019-11-14 18:42:36 -0500

    e17 2

--- e17/js/style.js
+++ e17/js/style.js
@@ -5,4 +5,6 @@ $(document).ready(function(){
   $('.f1').click(function() {
     $('.ff1').toggleClass('align-left');
   });
-});
+  $('.f3').click(function() {
+    $('.ss1, .ss2').toggleClass('disappear');
+  });
