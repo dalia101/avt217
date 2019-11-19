f0ec2d6c6d64028227c3fe5a23098144f2d85683
dalia101 <54603019+dalia101@users.noreply.github.com>
2019-11-19 14:30:10 -0500

    e17 done

--- e17/js/style.js
+++ e17/js/style.js
@@ -2,9 +2,21 @@ $(document).ready(function(){
 
   console.log('test');
   });
-  $('.f1').click(function() {
-    $('.ff1').toggleClass('align-left');
-  });
+  // $('.f').click(function() {
+  //   $('.ff1').toggleClass('align-left');
+  // });
   $('.f3').click(function() {
-    $('.ss1, .ss2').toggleClass('disappear');
+    $('.ss1, .ss2, .ss3, .ss4, .ss5, .ss6, .ss7').toggleClass('disappear');
+  });
+  $('.f1').click(function() {
+    $('.bb1, .bb2, .bb3, .bb4').toggleClass('switch');
+  });
+  $('.f1').click(function() {
+    $('.f1').toggleClass('switch2');
+  });
+  $('.f2, .f4').click(function() {
+    $('.ff2, .ff4').toggleClass('textsize');
+  });
+  $('.f5').click(function() {
+    $('.cc1, .cc2, .cc3, .cc4, .cc5, .cc6').toggleClass('size');
   });
